b0cb44de62269fe530cf4a216f0bb39c519584d3
BossTata <ngecell@gmail.com>
2025-10-03 01:03:00 +0200

    v1.5.1 - Add custom date selection for loans and payments for accurate historical reporting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tbfs-loan-manager-v18'; // Increment when updating
+const CACHE_NAME = 'tbfs-loan-manager-v19'; // Increment when updating
 const urlsToCache = [
   './',
   './index.html',
