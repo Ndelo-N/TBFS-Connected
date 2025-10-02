1f481918dd683800e5a7af97d38772c1f0612ecd
BossTata <ngecell@gmail.com>
2025-10-02 19:18:16 +0200

    Fix mobile tabs: Make tab bar horizontally scrollable - v1.2.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tbfs-loan-manager-v3'; // Increment when updating
+const CACHE_NAME = 'tbfs-loan-manager-v4'; // Increment when updating
 const urlsToCache = [
   './',
   './index.html',
